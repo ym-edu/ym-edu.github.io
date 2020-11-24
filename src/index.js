a4ccd320fb7c74c859177813200e5422a19bf5ef
nym <nym@nym.local>
2020-11-23 18:55:16 -0600

    test

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { run } from "./app/app.js"
+import "./main.scss"
+import { AlertService } from "./app/alert.service"
+import { ComponentService } from "./app/component.service"
+
+const alertService = new AlertService();
+const componentService = new ComponentService();
+
+run(alertService, componentService);
